d0d193402ae61e8a126626ed3bec29c472983d2c
akun <2969842877@qq.com>
2025-01-20 09:24:05 +0800

    [style] 查看代码缩进问题

--- IP/single-cycle-cpu/BR_JMP.v
+++ IP/single-cycle-cpu/BR_JMP.v
@@ -1,10 +1,10 @@
 module BR_JMP
 #(WIDTH =32)
 (
-	input wire               	CTRL_i_br_un,
-	input wire [WIDTH - 1:0] 	IDU_i_valA,
-	input wire [WIDTH - 1:0] 	IDU_i_valB,
-	output wire 				BR_JMP_o_br_eq,
+	input wire					CTRL_i_br_un,
+	input wire [WIDTH - 1:0]	IDU_i_valA,
+	input wire [WIDTH - 1:0]	IDU_i_valB,
+	output wire					BR_JMP_o_br_eq,
 	output wire 				BR_JMP_o_br_lt
 );
 
