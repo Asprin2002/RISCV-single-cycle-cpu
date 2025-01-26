9c02f170bd2bb8c9ad578e8664a58694377a0746
akun <2969842877@qq.com>
2025-01-26 13:58:24 +0800

    [dev] 对访存错误提供报错信息

--- simulator/src/sim/dpi.c
+++ simulator/src/sim/dpi.c
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <defs.h>
+#include <sys/types.h>
 #include "verilated_dpi.h" // For VerilatedDpiOpenVar and other DPI related definitions
 
 
@@ -19,9 +20,13 @@ extern "C" int dpi_mem_read(int addr, int len){
 		int time = get_time();
 		IFDEF(CONFIG_DIFFTEST, difftest_skip_ref());
 		return time;
-	}else{
+	}else if((u_int32_t)addr >= 0x80000000 && (u_int32_t)addr <= 0x8fffffff){
 		unsigned int data = pmem_read(addr, len);
 		return data;
+	}else{
+		printf("你的处理器将要访问的地址是[0x%x], 内存越界, 程序即将退出\n", addr);
+		npc_close_simulation();
+		exit(1);
 	}
 }
 extern "C" void dpi_mem_write(int addr, int data, int len){
@@ -30,8 +35,12 @@ extern "C" void dpi_mem_write(int addr, int data, int len){
 		printf("%c", ch);
 		fflush(stdout);
 		IFDEF(CONFIG_DIFFTEST, difftest_skip_ref());
-	}else{
+	}else if((u_int32_t)addr >= 0x80000000 && (u_int32_t)addr <= 0x8fffffff){
 		pmem_write(addr, len, data);
+	}else{
+		printf("你的处理器将要访问的地址是[0x%x], 内存越界, 程序即将退出\n", addr);
+		npc_close_simulation();
+		exit(1);
 	}
 }
 
